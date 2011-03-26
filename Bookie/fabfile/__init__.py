bf87765adde2f315d6d77ffd31d767c14aae8357
Richard Harding <rharding@mitechie.com>
2011-03-25 20:19:01 -0400

    Start the process of creating bootstrap config commands in fabric

--- Bookie/fabfile/__init__.py
+++ Bookie/fabfile/__init__.py
@@ -1,4 +1,5 @@
 import ConfigParser
+import os
 
 from fabric.api import run, sudo, hosts, local, cd, env, require, prompt
 from fabric.contrib.project import rsync_project
@@ -13,3 +14,33 @@ env.new_version_files = ["{project_name}/__init__.py".format(**env),
 from docs import *
 from database import *
 from tests import *
+
+# starter helpers
+def new_install(install_name):
+    """For a new install, create a new config file and write it out for them"""
+
+    # first check if we already have this defined
+    environment_file = os.path.join(os.path.dirname(__file__), 'environments.py')
+
+    current_env = open(environment_file).read()
+
+    if 'def {0}'.format(install_name) in current_env:
+        raise Exception("That install is already defined in environments.py")
+
+    new_env = """
+def {0}():
+    \"\"\"Environment settings for the dev environment\"\"\"
+    env.hosts = ['localhost']
+    env.ini_file = "{0}.ini"
+    env.ini = None
+
+"""
+
+    with open(environment_file, 'a+') as en_file:
+        en_file.write(new_env.format(install_name))
+
+    # we also need to create a .ini file for this install
+    ini_filname = "{0}.ini".format(install_name)
+
+    # need to cp the sample file over as the new ini_filename
+
