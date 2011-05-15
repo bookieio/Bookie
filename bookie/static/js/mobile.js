e8f54c6d26b38718421ff4a3fdfd0f53e06aed39
Richard Harding <rharding@mitechie.com>
2011-05-15 15:52:17 -0400

    Fix the url construction

--- bookie/static/js/mobile.js
+++ bookie/static/js/mobile.js
@@ -261,7 +261,7 @@ var bookie = (function ($b, $) {
             $('.bookmark_link').bind('click', function (ev) {
                 // the url we need to call is /redirect/hash_id
                 var hash_id = $(this).attr('data-hash'),
-                    url = app_url + "/redirect/" + hash_id;
+                    url = APP_URL + "/redirect/" + hash_id;
 
                 var newwindow = window.open(url, '_blank');
                 newwindow.focus();
