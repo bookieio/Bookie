eebf8073d4da915f8cbb3c9a74d91f067306f78d
Will Fuqua <wafuqua@gmail.com>
2011-05-19 00:03:04 -0400

    js version of python uuid algorithm

--- extensions/uuid/impl.py
+++ extensions/uuid/impl.py
@@ -3,22 +3,30 @@
 from hashlib import md5
 import uuid
 
-name = "google"
+name = "agoogle"
 namespace = uuid.NAMESPACE_URL
 
+class Dummy:
+    bytes = ""
+
 def python_impl():
-    return uuid.uuid3(namespace, name).int
+    return uuid.uuid3(Dummy(), name).int
 
 def manual_impl():
-    md5hash = md5(namespace.bytes + name).digest()
+    #md5hash = md5(namespace.bytes + name).digest()
+    md5hash = md5("" + name).digest()
     md5hash = md5hash[:16]
     uu = long(("%02x"*16) % tuple(map(ord, md5hash)), 16)
+    #print(uu)
     uu &= ~(0xc000 << 48L)
+    #print((uu))
     uu |= 0x8000 << 48L
+    #print((uu))
     uu &= ~(0xf000 << 64L)
+    #print((uu))
     uu |= 3 << 76L # version 3
+    #print((uu))
     return uu
-
 
 print(python_impl())
 print(manual_impl())
