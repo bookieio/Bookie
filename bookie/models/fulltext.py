149af3de4bedc45cd73cb1f387354c140b085f47
Richard Harding <rharding@mitechie.com>
2011-08-29 21:58:28 -0400

    Initial stab at testing/getting something working

--- bookie/models/fulltext.py
+++ bookie/models/fulltext.py
@@ -5,6 +5,7 @@ and API as we did in the importer
 
 """
 import logging
+import os
 from sqlalchemy import or_
 from sqlalchemy import text
 from sqlalchemy.orm import aliased
@@ -24,6 +25,8 @@ LOG = logging.getLogger(__name__)
 
 def get_fulltext_handler(engine):
     """Based on the engine, figure out the type of fulltext interface"""
+    return WhooshFulltext()
+
     if 'sqlite' in engine:
         return SqliteFulltext()
 
@@ -32,6 +35,61 @@ def get_fulltext_handler(engine):
 
     if 'pg' in engine or 'postgres' in engine:
         return PgSqlFulltext()
+
+from whoosh.fields import SchemaClass, TEXT, KEYWORD, ID
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.index import create_in
+from whoosh.index import open_dir
+from whoosh import qparser
+
+class BmarkSchema(SchemaClass):
+    bid = ID(stored=True)
+    description = TEXT
+    extended = TEXT
+    tags = KEYWORD
+    readable = TEXT(analyzer=StemmingAnalyzer())
+
+INDEX_NAME = 'bookie_index'
+if not os.path.exists(INDEX_NAME):
+    os.mkdir(INDEX_NAME)
+    WIX = create_in(INDEX_NAME, BmarkSchema)
+else:
+    WIX = open_dir(INDEX_NAME)
+
+
+def get_writer():
+    writer = WIX.writer()
+    return writer
+
+
+class WhooshFulltext(object):
+    """Implement the fulltext api using whoosh as a storage backend
+
+    """
+
+    def search(self, phrase, content=False, username=None, ct=10, offset=0):
+        """Implement the search, returning a list of bookmarks"""
+        with WIX.searcher() as search:
+            fields = ['description', 'extended', 'tags']
+            if content:
+                fields.append('readable')
+
+            parser = qparser.MultifieldParser(fields,
+                                           schema=WIX.schema,
+                                           group=qparser.OrGroup)
+            qry = parser.parse(phrase)
+            LOG.debug(qry)
+
+            res = search.search(qry)
+            LOG.debug(res)
+            ids = [r['bid'] for r in res]
+            LOG.debug('ids')
+            LOG.debug(ids)
+            qry = Bmark.query.filter(Bmark.bid.in_([r['bid'] for r in res]))
+            qry = qry.options(joinedload('hashed'))
+
+            return qry.all()
+
 
 
 class SqliteFulltext(object):
