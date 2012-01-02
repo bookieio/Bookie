c29308581a9e551eb702560311821b16d21cbfb3
Richard Harding <rharding@mitechie.com>
2012-01-02 13:27:21 -0500

    Get working the adding/removing. Move tags to own class

--- bookie/static/js/bookie/ytagcontrol.js
+++ bookie/static/js/bookie/ytagcontrol.js
@@ -21,31 +21,74 @@ YUI.add('bookie-tagcontrol', function (Y) {
         backspace:8
     };
 
+    function Tag(config) {
+        Tag.superclass.constructor.apply(this, arguments);
+    }
+
+    Tag.NAME = 'TagControllerTag';
+    Tag.ATTRS = {
+        'text': {},
+        'cssClass': {},
+        'parent': {}
+    };
+
+    Y.extend(Tag, Y.Base, {
+        _bind: function () {
+            // if clicked on, remove it
+            this.ui.on('click', this.destroy, this);
+        },
+
+        _buildui: function () {
+            var ui = Y.Node.create('<li/>');
+
+            ui.addClass(this.get('cssClass'));
+            ui.setContent(this.get('text'));
+
+            return ui;
+        },
+
+        destructor: function () {
+            Y.fire('tag:removed', {
+                target: this
+            });
+            // remove this node
+            this.ui.remove();
+        },
+
+        initializer: function (cfg) {
+            this.ui = this._buildui();
+            this._bind();
+        }
+    });
+
     ns.TagControl = Y.Base.create('bookie-tagcontrol', Y.Widget, [], {
-        tags: [],
         tpl: {
             main: '<div><ul><li><input/></li></ul></div>',
-            tag: '<li/>'
         },
 
         _add_tag: function () {
-            debugger;
             var input = this.ui.one('input'),
                 current_text = input.get('value'),
-                new_tag = Y.Node.create(this.tpl.tag);
+                new_tag = new Tag({
+                    text: current_text,
+                    cssClass: this.getClassName('item'),
+                    parent: this.ui
+                });
 
-            new_tag.addClass(this.getClassName('item'));
-            new_tag.setContent(current_text);
+            // keep this up
+            this.get('tags').push(new_tag);
 
             // add a new li element before the input one
             var parent = input.get('parentNode');
-            parent.get('parentNode').insertBefore(new_tag, parent);
+            parent.get('parentNode').insertBefore(new_tag.ui, parent);
 
             // clear the input
             input.set('value', '');
 
             // focus on the input
             input.focus();
+
+            this._sync_tags();
         },
 
         _bind_events: function () {
@@ -60,6 +103,8 @@ YUI.add('bookie-tagcontrol', function (Y) {
                 'input',
                 this
             );
+
+            Y.on('tag:removed', this._remove_tag, this);
         },
 
         _buildui: function () {
@@ -70,12 +115,40 @@ YUI.add('bookie-tagcontrol', function (Y) {
         },
 
         _parse_input: function (e) {
-            debugger;
             if (e.keyCode == keymap.space || e.keyCode == keymap.enter) {
                 // then handle the current input as a tag and clear for a new
                 // one
                 this._add_tag();
             }
+        },
+
+        _remove_tag: function (e) {
+            var t = e.target
+                tag = t.get('text');
+            Y.Array.find(this.get('tags'), function (item, index, list) {
+                debugger;
+                if (item.get('text') == tag) {
+                    var tlist = this.get('tags');
+                    tlist.splice(index, 1);
+                    this.set('tags', tlist);
+                }
+
+                return true;
+            }, this);
+        },
+
+        _sync_tags: function () {
+            // make sure we keep the tags in our list up to date with the
+            // input box
+            var tag_list = Y.Array.reduce(this.get('tags'), '', function (prev, cur, index, array) {
+                if (prev.length > 0) {
+                    return [prev, cur.get('text')].join(' ');
+                } else {
+                    return cur.get('text');
+                }
+            }, this);
+
+            this.get('srcNode').set('value', tag_list);
         },
 
         bindUI: function () {
@@ -86,7 +159,9 @@ YUI.add('bookie-tagcontrol', function (Y) {
             // first start out by hiding the initial input and placing our own
             // in it's place
             var target = this.get('srcNode')
-            target.hide();
+            // make the target a hidden field so it still gets passed to forms
+            // submitted
+            target.set('type', 'hidden');
 
             this._buildui();
             target.get('parentNode').insertBefore(this.ui);
@@ -97,8 +172,10 @@ YUI.add('bookie-tagcontrol', function (Y) {
         }
     }, {
         ATTRS: {
+            tags: {
+                value: []
+            }
         },
-
     });
 
 
@@ -394,5 +471,5 @@ YUI.add('bookie-tagcontrol', function (Y) {
 
 
 }, '0.1.0', { requires: [
-    'base', 'widget', 'handlebars'
+    'base', 'widget', 'handlebars', 'array-extras'
 ] });
