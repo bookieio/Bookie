ffcff3edeee77fecf3546a23eb8045eb916c2729
Richard Harding <rharding@mitechie.com>
2012-03-20 21:05:05 -0400

    fix typo, don't code on meds kids

--- bookie/routes.py
+++ bookie/routes.py
@@ -54,7 +54,7 @@ def build_routes(config):
     # matches based on the header
     # HTTP_X_REQUESTED_WITH
     # ajax versions are used in the mobile search interface
-    config.add_route("search_results_ajax", "results/*terms", xhr=Tru e)
+    config.add_route("search_results_ajax", "results/*terms", xhr=True)
     config.add_route("search_results_rest", "results/*terms")
     config.add_route("user_search_results_ajax",
                      "{username}/results*terms",
