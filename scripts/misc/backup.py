7c4c50cfee84f2775b66df7e474cbe454e65dac1
Rick Harding <rharding@mitechie.com>
2012-04-16 15:10:20 -0300

    Update scripts/misc/backup.py

--- scripts/misc/backup.py
+++ scripts/misc/backup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-from datetime import date
 import gzip
 import logging
 import os
 import urllib
+from datetime import date
+
 
 EXPORT_URL = "http://rick.bmark.us/export"
 BACKUP_DIR = '/home/rharding/bookie'
-
 LOG = logging.getLogger(__name__)
 BACKUP_FILE = "bookie_export_{0}".format(date.today().strftime('%Y_%m_%d'))
+
 
 if __name__ == "__main__":
     export = urllib.urlopen(EXPORT_URL)
