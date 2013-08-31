f0a77bb126a7349a6ba58a7dfb042e13587b5808
Ryan Kather <rkather@missionpenguin.com>
2013-08-31 15:48:39 -0400

    Fix null login submission testing and comment wording

--- bookie/tests/test_auth/__init__.py
+++ bookie/tests/test_auth/__init__.py
@@ -5,8 +5,6 @@ from nose.tools import ok_, eq_
 from pyramid import testing
 from unittest import TestCase
 
-# from bookie.lib import access
-# from bookie.models import DBSession
 
 LOG = logging.getLogger(__name__)
 
@@ -77,17 +75,20 @@ class TestAuthWeb(TestCase):
             "Should have 'Failed login' in the resp: " + str(res))
 
     def test_login_null(self):
-	"""Verify a null login"""
+        """Verify null login form submission fails"""
 
-	user_data = {'login': '',
-		     'password': '',
-		     'form.submitted': 'true'}
+        user_data = {
+            'login': '',
+            'password': '',
+            'form.submitted': 'true'
+        }
 
-	res = self.testapp.post('/login',
-				params=user_data)
-	eq_(res.status, "200 OK",
-	    msg='status is 200 OK, ' + res.status)
+        res = self.testapp.post('/login',
+                                params=user_data)
 
-	# should end up back at login with an error message
-	ok_('Failed login' in str(res),
-	    "Should have 'Failed login' in the resp: " + str(res))
+        eq_(res.status, "200 OK",
+            msg='status is 200 OK, ' + res.status)
+
+        # should end up back at login with an error message
+        ok_('Failed login' in str(res),
+            "Should have 'Failed login' in the resp: " + str(res))
