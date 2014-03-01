bf3218f9d125c9b4072fc99d108fe936578c79e0
Richard Harding <rharding@mitechie.com>
2014-02-28 22:57:41 -0500

    Fix the lowercase migration to work in other dbs

--- dbversions/versions/44dccb7b8b82_update_username_to_l.py
+++ dbversions/versions/44dccb7b8b82_update_username_to_l.py
@@ -13,14 +13,47 @@ down_revision = '9f274a38d84'
 from alembic import op
 import sqlalchemy as sa
 
+
 def upgrade():
     connection = op.get_bind()
     current_context = op.get_context()
     meta = current_context.opts['target_metadata']
     users = sa.Table('users', meta, autoload=True)
-    stmt = users.update().\
-	   values(username=sa.func.lower(users.c.username))
-    connection.execute(stmt)
+    bmarks = sa.Table('bmarks', meta, autoload=True)
+
+    try:
+        op.drop_constraint("bmarks_username_fkey", "bmarks")
+        print 'dropped constraint'
+    except sa.exc.OperationalError:
+        # If it's not supported then pass
+        pass
+
+    sel = sa.select([users])
+    for user in connection.execute(sel):
+        print 'updating for user: ' + user['username']
+        lowered = sa.func.lower(user['username'])
+
+        stmt = users.update().\
+            where(users.c.username == user['username']).\
+            values(username=lowered)
+        connection.execute(stmt)
+
+        stmt = bmarks.update().\
+            where(bmarks.c.username == user['username']).\
+            values(username=lowered)
+        connection.execute(stmt)
+        print 'done user: ' + user['username']
+
+    try:
+        op.create_foreign_key(
+            "bmarks_username_fkey", "bmarks",
+            "users", ["username"], ["username"])
+
+        print 'added constraint'
+    except sa.exc.OperationalError:
+        # If it's not supported then pass
+        pass
+
 
 def downgrade():
     pass
