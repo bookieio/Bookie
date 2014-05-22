f158093b2f38e29249edaede3299befaf2cd84ca
Richard Harding <rharding@mitechie.com>
2014-05-21 21:29:53 -0400

    Comment out the migration to get the db setup to interrogate

--- dbversions/versions/44dccb7b8b82_update_username_to_l.py
+++ dbversions/versions/44dccb7b8b82_update_username_to_l.py
@@ -15,46 +15,46 @@ import sqlalchemy as sa
 
 
 def upgrade():
-    connection = op.get_bind()
-    current_context = op.get_context()
-    meta = current_context.opts['target_metadata']
-    users = sa.Table('users', meta, autoload=True)
-    bmarks = sa.Table('bmarks', meta, autoload=True)
-
-    try:
-        op.drop_index("bmarks_username_fkey", "bmarks")
-        op.drop_constraint("bmarks_username_fkey", "bmarks", type="foreignkey")
-        print 'dropped constraint'
-    except sa.exc.OperationalError:
-        # If it's not supported then pass
-        pass
-
-    sel = sa.select([users])
-    for user in connection.execute(sel):
-        print 'updating for user: ' + user['username']
-        lowered = sa.func.lower(user['username'])
-
-        stmt = users.update().\
-            where(users.c.username == user['username']).\
-            values(username=lowered)
-        connection.execute(stmt)
-
-        stmt = bmarks.update().\
-            where(bmarks.c.username == user['username']).\
-            values(username=lowered)
-        connection.execute(stmt)
-        print 'done user: ' + user['username']
-
-    try:
-        op.create_foreign_key(
-            "bmarks_username_fkey", "bmarks",
-            "users", ["username"], ["username"])
-
-        print 'added constraint'
-    except sa.exc.OperationalError:
-        # If it's not supported then pass
-        pass
-
+    # connection = op.get_bind()
+    # current_context = op.get_context()
+    # meta = current_context.opts['target_metadata']
+    # users = sa.Table('users', meta, autoload=True)
+    # bmarks = sa.Table('bmarks', meta, autoload=True)
+    #
+    # try:
+    #     op.drop_index("bmarks_username_fkey", "bmarks")
+    #     op.drop_constraint("bmarks_username_fkey", "bmarks", type="foreignkey")
+    #     print 'dropped constraint'
+    # except sa.exc.OperationalError:
+    #     # If it's not supported then pass
+    #     pass
+    #
+    # sel = sa.select([users])
+    # for user in connection.execute(sel):
+    #     print 'updating for user: ' + user['username']
+    #     lowered = sa.func.lower(user['username'])
+    #
+    #     stmt = users.update().\
+    #         where(users.c.username == user['username']).\
+    #         values(username=lowered)
+    #     connection.execute(stmt)
+    #
+    #     stmt = bmarks.update().\
+    #         where(bmarks.c.username == user['username']).\
+    #         values(username=lowered)
+    #     connection.execute(stmt)
+    #     print 'done user: ' + user['username']
+    #
+    # try:
+    #     op.create_foreign_key(
+    #         "bmarks_username_fkey", "bmarks",
+    #         "users", ["username"], ["username"])
+    #
+    #     print 'added constraint'
+    # except sa.exc.OperationalError:
+    #     # If it's not supported then pass
+    #     pass
+    #
 
 def downgrade():
     pass
